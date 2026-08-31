1bcad1777a2ff7416878291bb5b25d85f62d47a5
rickyjdavies18-design <rickyjdavies18@gmail.com>
2026-08-31 15:01:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import sqlite3
 
 DB = "ria.db"
 app = FastAPI(title="Ria - Tattoo Assistant")
-
+app.mount("/static", StaticFiles(directory="."), name="static")
 
 def db():
     conn = sqlite3.connect(DB)
